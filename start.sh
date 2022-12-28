1f8be90e5e6c2a9b3374dcba55958805273fca30
Vihara Senindu <110663030+viharasenindu@users.noreply.github.com>
2022-12-28 17:06:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/SECCOMPLEX/Netflixlkbot.git /Netflixlkbot
+  git clone https://github.com/viharasenindu/Newmovie
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Netflixlkbot
